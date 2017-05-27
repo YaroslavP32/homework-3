9bbe062ec812255c1b38ac33a038202c5b8e4ea9
YaroslavP32 <yaroslavp32@gmail.com>
2017-05-27 16:31:59 +0300

    Added file

---
+++ main.js
@@ -0,0 +1,17 @@
+1.  db.test.find({'scores.score': {$gt: 87, $lt: 93}}).pretty()
+
+2.  db.test.aggregate([{$unwind: "$scores"}, {$match: {'scores.type': "exam", 'scores.score': {$gt: 90}}}])
+
+3.  db.test.update({"name": "Dusti Lemmond"}, {$set: {"accepted": "true"}}, {multi: true})
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+              
